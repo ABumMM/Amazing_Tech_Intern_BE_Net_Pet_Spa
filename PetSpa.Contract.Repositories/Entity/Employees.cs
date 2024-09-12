35341d760de039e9ede832d969ce7d771a50a5e1
bumm <anhnguyen20031609@gmail.com>
2024-09-12 22:01:05 +0700

    Entity Review

--- PetSpa.Contract.Repositories/Entity/Employees.cs
+++ PetSpa.Contract.Repositories/Entity/Employees.cs
@@ -14,11 +14,11 @@ namespace PetSpa.Contract.Repositories.Entity
         public Guid Id { get; set; }
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string PhoneNPhoneNumber {  get; set; }
+        public string PhoneNPhoneNumber {  get; set; } = string.Empty ;
         public DateTime DayofBirth { get; set; }
         public string Address { get; set; }
         public double? Salary { get; set; }
+        
 
-        //thêm khóa ngoại sau
     }
 }
--- PetSpa.Contract.Repositories/obj/Debug/net8.0/PetSpa.Contract.Repositories.AssemblyInfo.cs
+++ PetSpa.Contract.Repositories/obj/Debug/net8.0/PetSpa.Contract.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetSpa.Contract.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+716bc033ab6509d534a09d6143a17f180b191477")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902ee9bd0cc265d561e2b7c3db91b82e48c26490")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetSpa.Contract.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetSpa.Contract.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
