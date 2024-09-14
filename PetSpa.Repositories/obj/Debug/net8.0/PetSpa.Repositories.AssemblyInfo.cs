16f7f0c19eea82bac9f35657277c2c5583f2108b
bumm <anhnguyen20031609@gmail.com>
2024-09-14 15:30:09 +0700

    fix lỗi deloy git

--- PetSpa.Repositories/obj/Debug/net8.0/PetSpa.Repositories.AssemblyInfo.cs
+++ PetSpa.Repositories/obj/Debug/net8.0/PetSpa.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetSpa.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693dae1202cee8a01befc4dbafae3e9ce3f56dfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0e838571a9815cbc204965f22e09a8deb31010")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetSpa.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetSpa.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
