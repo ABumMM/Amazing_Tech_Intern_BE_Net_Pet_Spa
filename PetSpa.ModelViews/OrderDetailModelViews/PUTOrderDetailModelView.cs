d1c643bdd814b95cfaf9019052d6b359923dfcab
Lngqminh <lngqminh@gmail.com>
2024-09-23 11:22:39 +0700

    CRUD OrderDetail 23/09/2024

---
+++ PetSpa.ModelViews/OrderDetailModelViews/PUTOrderDetailModelView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSpa.ModelViews.OrderDetailModelViews
+{
+    public class PUTOrderDetailModelView
+    {
+        public required string? Id { get; set; }
+        public int? Quantity { get; set; }
+        public string? Status { get; set; }
+        public decimal? Price { get; set; }
+        public string? LastUpdatedBy { get; set; }
+        public DateTimeOffset LastUpdatedTime { get; set; }
+    }
+}
