a7fe3ad7a014bdda2a64c74b09db9bcd0d056a32
Lngqminh <lngqminh@gmail.com>
2024-10-11 22:52:08 +0700

    Mapper OrderDetail 11/10/2024

--- PetSpa.ModelViews/OrderDetailModelViews/PUTOrderDetailModelView.cs
+++ PetSpa.ModelViews/OrderDetailModelViews/PUTOrderDetailModelView.cs
@@ -11,7 +11,7 @@ namespace PetSpa.ModelViews.OrderDetailModelViews
         public required string Id { get; set; }
         public int? Quantity { get; set; }
         public string? Status { get; set; }
-        public decimal Price { get; set; } = 0;
+        //public decimal Price { get; set; } = 0;
         public string? LastUpdatedBy { get; set; }
         public DateTimeOffset LastUpdatedTime { get; set; }
     }
