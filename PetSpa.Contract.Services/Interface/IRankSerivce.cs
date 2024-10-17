c37439d56e6f8dcea6b8efc94ccf4dc0902869d1
LeeMinKaHo <leminhkhoa5241@gmail.com>
2024-10-17 17:58:52 +0700

    CRUD Rank

---
+++ PetSpa.Contract.Services/Interface/IRankSerivce.cs
@@ -0,0 +1,19 @@
+﻿using PetSpa.Core.Base;
+using PetSpa.ModelViews.RankModelViews;
+using PetSpa.ModelViews.ReviewModelViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSpa.Contract.Services.Interface
+{
+    public interface IRankSerivce
+    {
+        Task<BasePaginatedList<GetRankViewModel>> GetAll(int pageNumber, int pageSize);
+        Task Add(PostRankViewModel rank);
+        Task Update(string RankID, UpdateViewModel UpdateRank);
+        Task Delete(string rankID);
+    }
+}
