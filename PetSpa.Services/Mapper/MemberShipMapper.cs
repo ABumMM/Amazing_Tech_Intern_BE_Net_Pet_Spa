c0e4b3186498fe2e08a7ecc20c49c9fc5e289219
LeeMinKaHo <leminhkhoa5241@gmail.com>
2024-10-18 20:09:21 +0700

    delete Name

--- PetSpa.Services/Mapper/MemberShipMapper.cs
+++ PetSpa.Services/Mapper/MemberShipMapper.cs
@@ -10,8 +10,6 @@ namespace PetSpa.Services.Mapper
             //MemberShip sang GETMemberShipModelView
             CreateMap<MemberShips, GETMemberShipModelView>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-            
             .ForMember(dest => dest.TotalSpent, opt => opt.MapFrom(src => src.TotalSpent))
             .ForMember(dest => dest.CreatedTime, opt => opt.MapFrom(src => src.CreatedTime))
             .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.CreatedBy));
