544eebc2d0954d90235de9f2e3f0acbcd7632b65
LeeMinKaHo <leminhkhoa5241@gmail.com>
2024-11-15 22:47:32 +0700

    add razor pages

---
+++ PetSpaBE.Razor/Pages/Services/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PetSpa.Contract.Services.Interface;
+using PetSpa.Core.Base;
+using PetSpa.ModelViews.RankModelViews;
+using PetSpa.ModelViews.ServiceModelViews;
+
+namespace PetSpaBE.Razor.Pages.Services
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IServicesService _serviceService;
+        public IndexModel(IServicesService ServicesService)
+        {
+            _serviceService = ServicesService;
+        }
+
+		public BasePaginatedList<ServiceResposeModel> lst_Services { get; private set; }
+
+		// Thuộc tính để lưu danh sách rank
+
+		public async Task OnGet(int page = 1 , int size = 6 ,string type = "Customer")
+        {
+            lst_Services=  await _serviceService.GetAll(pageNumber:1,pageSize:6);
+            type = type;
+          
+        }
+    }
+}
