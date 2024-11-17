35a7e3be7d8eeff090ba6fffe1e924b65de17494
LeeMinKaHo <leminhkhoa5241@gmail.com>
2024-11-18 00:20:06 +0700

    service-rank

---
+++ PetSpaBE.Razor/Pages/Ranks/Update.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PetSpa.Contract.Repositories.Entity;
+using PetSpa.Contract.Services.Interface;
+using PetSpa.ModelViews.RankModelViews;
+using PetSpa.ModelViews.ServiceModelViews;
+using PetSpa.Services.Service;
+
+namespace PetSpaBE.Razor.Pages.Ranks
+{
+    public class UpdateModel : PageModel
+    {
+        private readonly IRankService _rankSerivce;
+		public GetRankViewModel rank { get; private set; }
+
+		public UpdateModel(IRankService rankSerivce)
+        {
+            _rankSerivce = rankSerivce;
+		}
+		public async Task<IActionResult> OnGet(string id)
+		{
+			// Get the service by ID to pre-populate the form for editing
+			rank = await _rankSerivce.GetByID(id); // Ensure you have a method to get the service by ID
+			if (rank == null)
+			{
+				return NotFound();
+			}
+
+
+			
+			return Page();
+		}
+    }
+}
