9b31dea32d2776a35ffb1f686184e86a71ab7939
Chiêm Hồng Hưng <154119588+hungi03@users.noreply.github.com>
2024-11-18 13:52:47 +0700

    update package

--- PetSpaBE.Razor/Pages/Package/Detail.cshtml.cs
+++ PetSpaBE.Razor/Pages/Package/Detail.cshtml.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PetSpa.Contract.Repositories.Entity;
 using PetSpa.Contract.Services.Interface;
+using PetSpa.ModelViews.PackageModelViews;
+using PetSpa.ModelViews.PackageServiceModelViews;
+using PetSpa.ModelViews.RankModelViews;
+using PetSpa.ModelViews.ReviewModelViews;
+using System.Drawing.Printing;
 
 namespace PetSpaBE.Razor.Pages.Package
 {
@@ -8,7 +14,10 @@ namespace PetSpaBE.Razor.Pages.Package
     {
         private readonly IPackageService _packageService;
         private readonly IReviewService _reviewService;
+        public GETPackageModelView Package { get;  set; }
+        public List<GETPackageServiceModelView> lstService { get; set; }
 
+        public List<GETReviewModelViews> Reviews { get; set; }
         public DetailModel(IPackageService packageService, IReviewService reviewService)
         {
             _packageService = packageService;
@@ -21,7 +30,10 @@ namespace PetSpaBE.Razor.Pages.Package
                 return NotFound();
             }
 
-           var Package = await _packageService.GetServicesByPackageId(Id);
+            Package = await _packageService.GetById(Id);
+            lstService = await _packageService.GetServicesByPackageId(Id);
+            var reviews = await _reviewService.GetAllReviewsInPackage(Id, 1, 10);
+            Reviews = reviews.Items.ToList();
             if (Package == null)
             {
                 return NotFound();
